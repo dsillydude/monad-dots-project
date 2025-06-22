fe74327081698071dfdc2d1edd0a10879aaea3fe
Zeus <dsillydude@gmail.com>
2025-06-22 10:22:24 -0700

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -6,21 +6,21 @@ export async function GET(request: NextRequest) {
     `${request.nextUrl.protocol}//${request.nextUrl.host}`;
   
   const farcasterConfig = {
-    accountAssociation: {
-      header: "",
-      payload: "",
-      signature: ""
-    },
+     "accountAssociation": {
+    "header": "eyJmaWQiOjQyMDEwOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDExQzNCMkZlNUFiMjgxNWE5RTBGREVmODFGM2NiRDlBZjQ5MDJEMDQifQ",
+    "payload": "eyJkb21haW4iOiJtb25hZC1kb3RzLXByb2plY3QudmVyY2VsLmFwcCJ9",
+    "signature": "MHhjYmY3NGI4YTc3ZGViZDhlYzdjZjNlZjMyMjM5NTBmOTRkOTlhNTc5ZGM3NGI4ZjA3NjRlZmNkNzhkZTk1ODhkNDgxYzJjN2I3Mzg3MTM2ZmEzOWQ5ODllOGM0ZGQzZDJjZGVkZTlkYWJjMDMxYTIzZmYyMjMwMWUxM2Q0MzliZDFi"
+  },
     frame: {
       version: "1",
-      name: "Monad Dots - Two Dots Inspired Puzzle Game",
+      name: "MonDots - Two Dots Inspired Puzzle Game",
       iconUrl: `${appUrl}/images/icon.png`,
       homeUrl: `${appUrl}`,
       imageUrl: `${appUrl}/images/feed.png`,
       screenshotUrls: [],
       tags: ["monad", "farcaster", "miniapp", "game", "puzzle", "dots"],
       primaryCategory: "games",
-      buttonTitle: "Play Monad Dots",
+      buttonTitle: "Play MonDots",
       splashImageUrl: `${appUrl}/images/splash.png`,
       splashBackgroundColor: "#7c3aed"
     }
